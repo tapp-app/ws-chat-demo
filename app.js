f1214c8e0f9a2b69e4dc48aff22a3d4a058e65ba
Daniel Chen <dyc5828@gmail.com>
2015-08-08 21:51:37 -0700

    rooms

--- app.js
+++ app.js
@@ -11,15 +11,41 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/chat.html');
 });
 
-io.on('connection', function(socket) {
-	console.log('a user connected');
+// io.on('connection', function(socket) {
+// 	console.log('a user connected');
 
-	socket.on('chat message', function(msg) {
-		console.log('message: ' + msg);
-		io.emit('chat message', msg);
+// 	socket.on('chat message', function(msg) {
+// 		console.log(msg)
+// 		io.emit('chat message', msg);
+// 	});
+
+// 	socket.on('disconnect', function() {
+// 		console.log('user disconnected');
+// 	});
+// });
+
+var chat = io.of('/chat');
+
+chat.on('connection', function(socket) {
+	console.log('user connected to chat');
+	socket.emit('joined', 'You have joined chat');
+
+	socket.join('room', function() {
+		console.log('user joined room');
+		console.log(socket.rooms);
 	});
+	chat.to('room').emit('joined', 'you have joined a room.');
+
+	socket.on('message', function(data) {
+		console.log(data);
+		chat.emit('message', data);
+	})
+
+	// socket.leave('room', function() {
+	// 	console.log('user left room');
+	// });
 
 	socket.on('disconnect', function() {
-		console.log('user disconnected');
+		console.log('user disconnected chat');
 	});
 });
